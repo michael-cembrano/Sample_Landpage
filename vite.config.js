e565acb5090492c66024bb85feea46b3cfdc5034
Michael Cembrano <stmicheal77@gmail.com>
2025-07-27 16:22:43 +0800

    updated vite. config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Sample_Landpage/",
   server: {
     port: 3000,
     open: true
@@ -13,6 +14,6 @@ export default defineConfig({
     assetsDir: 'assets',
     sourcemap: false,
     minify: 'terser',
-    base: "/Sample_Landpage/"
+    
   }
 })
